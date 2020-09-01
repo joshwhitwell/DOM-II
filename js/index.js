65235399bf34a880fa2422aabf0006dc61a8559a
Joshua Whitwell <josh.whitwell@gmail.com>
2020-09-01 19:03:40 -0400

    added event listeners 6-10 to index.js

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ Array.from(document.querySelectorAll('h1, h2, h4')).forEach(element => {
 })
 
 // 2. Change Font Size with Keydown//
+
 const allText = Array.from(document.querySelectorAll('h1, h2, h3, h4, p'))
 
 document.addEventListener('keydown', event => {
@@ -50,6 +51,63 @@ Array.from(document.links).forEach(element => {
         event.target.style.color = ''
         event.target.style.fontSize = ''})
 })
+
+// 6. Hide/Show Sections on Double Click //
+const destinationDivs = Array.from(document.querySelectorAll('.destination h4, .destination p'))
+
+Array.from(document.querySelectorAll('.btn')).forEach(element => {
+    element.addEventListener('dblclick', event => {
+        for (let i = 0; i < destinationDivs.length; i++)
+        if (destinationDivs[i].style.display === 'none') {
+            destinationDivs[i].style.display = ''
+        } else {
+            destinationDivs[i].style.display = 'none'
+        }
+    })
+})
+
+// 7. Change Image Size on Window Resize //
+
+const images = Array.from(document.querySelectorAll('img'))
+
+window.addEventListener('resize', event => {
+    images.forEach(element => {
+        if (element.style.width === '50%') {
+            element.style.width = ''
+        } else {
+            element.style.width = '50%'
+        }
+    })
+})
+
+// 8. Alert Copy Text //
+
+document.addEventListener('copy', event => {
+    let selection = document.getSelection()
+    alert(`You copied "${selection}" to the clipboard`)
+})
+
+// 9. Change Background on Scroll //
+document.addEventListener('scroll', event => {
+    if (document.body.style.backgroundColor === '') {
+        document.body.style.backgroundColor = 'red'
+    } else {
+        document.body.style.backgroundColor = ''
+    }
+})
+
+// 10. Nest stopPropagation and Nav Items preventDefault Using Click//
+const nav = document.querySelector('nav')
+nav.addEventListener('click', event => {
+    event.target.style.backgroundColor = 'dodgerblue'
+})
+
+Array.from(document.querySelectorAll('a')).forEach(element => element.addEventListener('click', event => {
+    event.target.style.color = 'green'
+    event.stopPropagation()
+    event.preventDefault()
+}))
+
 
 
 
